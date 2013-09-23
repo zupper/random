2eebfbd8fdaf74d4879e59424fd024d4bda96e71
shade <dimitar@shadez.info>
2013-09-23 15:36:18 +0300

    Work on loading game data

--- mh.py
+++ mh.py
@@ -23,6 +23,15 @@ class MH:
 	password = None
 
 	proxies = None
+
+	all_locations = None
+	all_bait = None
+
+	cache_dir = ".mh"
+	fb_access_token_file = "%s/fb_access_token" % cache_dir
+	mh_access_token_file = "%s/mh_access_token" % cache_dir
+	bait_json_file = "%s/bait.json" % cache_dir
+	locaitons_json_file = "%s/locations.json" % cache_dir
 	
 	# state vars
 	last_big_delay_horns_elapsed = 0;
@@ -32,20 +41,19 @@ class MH:
 		time = datetime.now().strftime("%H:%M:%S")
 		print "[%s] %s" % (time, text)
 	
-	def get_access_token(self, refresh=False):
+	def get_fb_token(self, refresh=False):
 		login_url = "https://m.facebook.com/login.php?skip_api_login=1&api_key=10337532241&signed_next=1&next=https%3A%2F%2Fm.facebook.com%2Fdialog%2Foauth%3Fredirect_uri%3Dfbconnect%253A%252F%252Fsuccess%26display%3Dtouch%26type%3Duser_agent%26client_id%3D10337532241%26ret%3Dlogin&cancel_uri=fbconnect%3A%2F%2Fsuccess%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied&display=touch&_rdr"
-		local_file = '.fb_access_token'
 	
-		if refresh or not os.path.exists(local_file):
+		if refresh or not os.path.exists(self.fb_access_token_file):
 			print "Logging you in to facebook..."
 			webbrowser.open(login_url)
 			access_token = raw_input('Access Token: ')
-			open(local_file,'w').write(access_token)
+			open(self.fb_access_token_file,'w').write(access_token)
 
 			print "Token saved for future use"
 			print ""
 		else:
-			access_token = open(local_file).read()
+			access_token = open(self.fb_access_token_file).read()
 		
 		return access_token
 
@@ -85,18 +93,17 @@ class MH:
 		return response['login_token']
 
 	def get_login_code(self, username, refresh=False):
-		local_file = '.mh_login_code'
 
-		if refresh or not os.path.exists(local_file):
+		if refresh or not os.path.exists(self.mh_access_token_file):
 			login_code = self.perform_login(username)
 
-			open(local_file,'w').write("%s:%s" % (username, login_code))
+			open(self.mh_access_token_file,'w').write("%s:%s" % (username, login_code))
 
 			print "Token saved for future use"
 			print ""
 
 		else:
-			contents = open(local_file).read().rstrip('\r\n').split(":")
+			contents = open(self.mh_access_token_file).read().rstrip('\r\n').split(":")
 			stored_username = contents[0]
 			stored_code = contents[1]
 
@@ -110,7 +117,17 @@ class MH:
 	def get_game_version(self):
 		self.tprint("[I] Getting game version...")
 		
-		response = requests.post("https://www.mousehuntgame.com/api/info", data={"game_version": "null"}, proxies=self.proxies)
+		headers = {
+			'User-Agent':		'Mozilla/5.0 (Linux; U; Android 2.3.3; en-en; HTC Desire Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+			'Accept':			'application/json, text/javascript, */*; q=0.01',
+			'Content-Type':		'application/x-www-form-urlencoded',
+			'X-Requested-With':	'com.hitgrab.android.mousehunt',
+			'Accept-Encoding': 	'gzip,deflate',
+			'Accept-Language':	'en-US',
+			'Accept-Charset':	'utf-8, iso-8859-1, utf-16, *;q=0.7'
+		}
+
+		response = requests.post("https://www.mousehuntgame.com/api/info", data={"game_version": "null"}, proxies=self.proxies, headers=headers)
 		session_id = response.cookies["PHPSESSID"]
 
 		response =  response.text.split("\r\n")
@@ -126,6 +143,66 @@ class MH:
 		else:
 			print "API responded weirdly: %s" % data
 			return None
+
+	def get_game_data(self, url):
+		headers = {
+			'User-Agent':		'Mozilla/5.0 (Linux; U; Android 2.3.3; en-en; HTC Desire Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+			'Accept':			'application/json, text/javascript, */*; q=0.01',
+			'Content-Type':		'application/x-www-form-urlencoded',
+			'X-Requested-With':	'com.hitgrab.android.mousehunt',
+			'Accept-Encoding': 	'gzip,deflate',
+			'Accept-Language':	'en-US',
+			'Accept-Charset':	'utf-8, iso-8859-1, utf-16, *;q=0.7'
+		}
+		cookies = {
+			'PHPSESSID': self.session_id
+		}
+		params = {
+			'v':				'2',
+			'client_id':		'Cordova%%3AAndroid',
+			'client_version':	'0.12.4',
+			'game_version':		self.game_version,
+		}
+
+		response = requests.post(url, headers=headers, data=params, cookies=cookies, proxies=self.proxies)
+		response = json.loads(response.text)
+
+		return response
+
+	def get_baits(self, refresh=False):
+		items_url = "https://www.mousehuntgame.com/api/get/item/all"
+
+		bait = []
+
+		if refresh or not os.path.exists(self.bait_json_file):
+
+			objects = self.get_game_data(items_url)
+
+			# filter by "classification": "bait" to get only the cheese
+			for item in objects:
+				if item['classification'] == 'bait':
+					bait.append(item)
+
+			open(self.bait_json_file, 'w').write(json.dumps(bait))
+		else:
+			raw_json = open(self.bait_json_file, 'w'),read()
+			bait = json.loads(raw_json)
+
+		return bait
+
+	def get_locations(self, refresh=False):
+		locations_url = "https://www.mousehuntgame.com/api/get/environment/all"
+
+		locations = []
+
+		if refresh or not os.path.exists(self.locaitons_json_file):
+			locations = self.get_game_data(locations_url)
+			open(self.locaitons_json_file, 'w').write(json.dumps(locations))
+		else:
+			raw_json = open(self.locaitons_json_file, 'w'),read()
+			locations = json.loads(raw_json)
+
+		return locations
 
 	def exit_error(self, reason):
 		print "Don't know what happened, see the reason below:"
@@ -220,7 +297,7 @@ class MH:
 			
 			elif response.status == "login":
 				self.tprint("[E] Session has expired. Reauthenticating...")
-				self.access_token = self.get_access_token(True)		# refreshing the expired access token
+				self.access_token = self.get_fb_token(True)		# refreshing the expired access token
 				
 				# have a small delay before trying to sound again
 				next_delay = random.randint(1, 10)
@@ -271,17 +348,19 @@ class MH:
 		random.seed()
 		
 		self.game_version = self.get_game_version()
+		self.all_bait = self.get_baits()
+		self.all_locations = self.get_locations()
+
+		# sleeping for a while to avoid having the two calls performed simultaneously
+		initial_delay = random.randint(5, 20)
+		self.tprint("[I] Sleeping for %s to avoid having the initial calls too close together." % initial_delay)
+		time.sleep(initial_delay)
 
 		if username is not None:
 			self.username = username
 			self.access_token = self.get_login_code(username)
 		else:
-			self.access_token = self.get_access_token()
-		
-		# sleeping for a while to avoid having the two calls performed simultaneously
-		initial_delay = random.randint(5, 20)
-		self.tprint("[I] Sleeping for %s to avoid having the two calls too close together." % initial_delay)
-		time.sleep(initial_delay)
+			self.access_token = self.get_fb_token()
 		
 		self.tprint("[I] Ready to hunt")
 		
