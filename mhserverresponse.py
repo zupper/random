d9da14c71c635213887bfc1e9e2e342d9d34be30
shade <dimitar@shadez.info>
2014-05-07 00:51:34 +0300

    Improved parsing of server response

--- mhserverresponse.py
+++ mhserverresponse.py
@@ -27,50 +27,37 @@ class MHServerResponse:
 	
 	data = None
 	
-	def __init__(self, raw_response):
-		response = raw_response.split("\r\n")
+	def __init__(self, response):
+		response = json.loads(response.rstrip())
 		
-		valid = False
-		for line in response:
-			if line.startswith("{"):
-				response = line
-				valid = True
+		if "error" in response:
 		
-		if not valid:
-			self.status = "error"
-			self.error = "Response doesn't contain JSON."
-			self.data = raw_response
+			if response['error']['code'] == 100:
+				self.status = "login"
+				self.error = response['error']['message']
+			elif response['error']['code'] == 400:
+				self.status = "warn"
+				self.error = response['error']['message']
+			elif response['error']['code'] == 80:
+				self.status = "update"
+				self.error = "Need game version update"
 		else:
-			response = json.loads(response)
-			
-			if "error" in response:
-			
-				if response['error']['code'] == 100:
-					self.status = "login"
-					self.error = response['error']['message']
-				elif response['error']['code'] == 400:
-					self.status = "warn"
-					self.error = response['error']['message']
-				elif response['error']['code'] == 80:
-					self.status = "update"
-					self.error = "Need game version update"
-			else:
-				self.status = "ok"
-			
-			if "user" not in response:
-				self.status = "error"
-				self.error = "No user object present. Somethign is wrong"
-				self.data = raw_response
+			self.status = "ok"
+		
+		if "user" not in response:
+			self.status = "error"
+			self.error = "No user object present. Somethign is wrong"
+			self.data = raw_response
 
-			else:
-				self.data = {
-						"have_bait": response["user"]["trap"]["bait_id"] is not None,
-						"time_to_horn": response["user"]["next_activeturn_seconds"],
-						"catch": {
-							"status": response["user"]["trap"]["last_activity"]["class_name"],
-							"mouse": response["user"]["trap"]["last_activity"]["mouse"],
-							"gold": response["user"]["trap"]["last_activity"]["gold"],
-							"points": response["user"]["trap"]["last_activity"]["points"],
-							"loot": response["user"]["trap"]["last_activity"]["loot"]
-						}
+		else:
+			self.data = {
+					"have_bait": response["user"]["trap"]["bait_id"] is not None,
+					"time_to_horn": response["user"]["next_activeturn_seconds"],
+					"catch": {
+						"status": response["user"]["trap"]["last_activity"]["class_name"],
+						"mouse": response["user"]["trap"]["last_activity"]["mouse"],
+						"gold": response["user"]["trap"]["last_activity"]["gold"],
+						"points": response["user"]["trap"]["last_activity"]["points"],
+						"loot": response["user"]["trap"]["last_activity"]["loot"]
 					}
+				}
